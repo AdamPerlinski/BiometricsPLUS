d390ca9256d19d8dc1808768e68ac59f8123e692
Adam Perlinski <foxtheowner@gmail.com>
2018-02-22 18:39:09 +0100

    hi

---
+++ WpfApp2/Model/Mainviewmodel.cs
@@ -0,0 +1,23 @@
+﻿namespace WpfApp2
+{
+    using System;
+
+    using OxyPlot;
+    using OxyPlot.Series;
+
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Result" };
+            this.MyModel.Series.Add(new FunctionSeries((Math.Cos), 0, 10, 0.1, "cos(x)"));
+        }
+
+        private void ToCode(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlotModel MyModel { get; private set; }
+    }
+}
